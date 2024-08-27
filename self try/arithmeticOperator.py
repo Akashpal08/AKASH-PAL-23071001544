e4a193739dbe689624212d86d65613c2cbabfa31
akashpal08 <akashpal28042005@gmail.com>
2024-08-27 14:26:24 +0530

    PYTHON PROGARAMING

---
+++ self try/arithmeticOperator.py
@@ -0,0 +1,36 @@
+# Use different types of arithmetic operator
+a=4/2
+d=7%2
+b=3**4
+c=4//3
+e=8-2
+f=5+5
+print(a,d,b,c,e,f)
+
+
+# Using arithmetic operator shortcut method 
+a**=3
+b%=10
+print(a,b)
+
+# floor division operator
+print(10//3)
+print(-10//3)
+print(10//-3)
+print(-10//-3)
+print(3//10)
+print(3//-10)
+print(-3//10)
+print(-3//-10)
+
+#  floor Modulas operator
+print(10%3)
+print(-10%3)
+print(10%-3)
+print(-10%-3)
+print(3%10)
+print(-3%10)
+print(3%-10)
+print(-3%-10)
+
+#
---
+++ self try/variables.py
@@ -0,0 +1,21 @@
+# find which value will be shown in output
+a=25
+a=31.4
+a='Hi'
+print(a)
+
+# find which type of  function
+a='akash'
+print(type(a))
+
+# print with space
+a=10
+pi=3.14
+name='sanjay'
+print(a,pi,name)
+
+# using seprator
+print("hello","world",sep="-")
+print("you","you not",sep="&")
+print("hi","by",sep="/")
+print("akash","pal",sep="#")
